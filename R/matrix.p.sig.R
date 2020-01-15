22d229d39d9ae6f5c63757967f38912cbc77c1fa
Vanderlei Julio Debastiani <vanderleidebastiani@yahoo.com.br>
2020-01-15 21:00:02 +0000

    version 1.0.7

--- R/matrix.p.sig.R
+++ R/matrix.p.sig.R
@@ -1,104 +1,62 @@
 #' @rdname pcps.sig
 #' @encoding UTF-8
 #' @export
-matrix.p.sig<-function(comm, phylodist, envir, analysis = c("adonis", "mantel"), method = "bray", squareroot = TRUE, method.envir = "euclidean", runs = 999, parallel = NULL, newClusters = TRUE, CL =  NULL){
-  RES <- list(call = match.call())
-  Analysis <- c("adonis", "mantel")
-  analysis <- pmatch(analysis, Analysis)
-  if (length(analysis) != 1 | (is.na(analysis[1]))) {
-    stop("\n Invalid analysis. Only one argument is accepted in analysis \n")
+matrix.p.sig <- function (comm, phylodist, envir, checkdata = TRUE, FUN, runs = 999, parallel = NULL, newname = "pcps", ...) 
+{
+  res <- list(call = match.call())
+  if (inherits(comm, "metacommunity.data")) {
+    if (!missing(phylodist)) {
+      stop("\n When you use an object of class metacommunity.data the argument phylodist must not be specified. \n")
+    }
+    phylodist <- comm$phylodist
+    envir <- comm$environmental
+    comm <- comm$community
   }
-  envir <- as.matrix(envir)
-  p.matrix <- SYNCSA::matrix.p(comm, phylodist, notification = FALSE)$matrix.P
-  p.dist <- vegan::vegdist(p.matrix, method = method)
-  if (squareroot) {
-    p.dist <- sqrt(p.dist)
+  list.warning <- list()
+  if(checkdata){
+    organize.temp <- organize.pcps(comm, phylodist = phylodist, envir = envir, check.comm = TRUE)
+    if(!is.null(organize.temp$stop)){
+      organize.temp$call <- match.call()
+      return(organize.temp)
+    }
+    list.warning <- organize.temp$list.warning
+    comm <- organize.temp$community
+    phylodist <- organize.temp$phylodist
+    envir <- organize.temp$environmental
+    
+    # envir nao conferido
+    
   }
-  if(!is.null(CL)){
-    parallel <- length(CL)
-    if(analysis == 1){
-      mod.obs <- vegan::adonis(p.dist~envir,permutations=runs, parallel = CL)
-      statistic.obs <- mod.obs$aov.tab$F.Model[1]
-      p.site.shuffle <- mod.obs$aov.tab$"Pr(>F)"[1]
-    }
-    if(analysis == 2){
-      env.dist <- vegan::vegdist(envir, method = method.envir)
-      mod.obs <- vegan::mantel(p.dist, env.dist, permutations = runs, parallel = CL)
-      statistic.obs <- mod.obs$statistic
-      p.site.shuffle <- mod.obs$signif
-    }
-  } else {
-    if(is.null(parallel)){
-      if(analysis == 1){
-        mod.obs <- vegan::adonis(p.dist~envir, permutations = runs)
-        statistic.obs <- mod.obs$aov.tab$F.Model[1]
-        p.site.shuffle <- mod.obs$aov.tab$"Pr(>F)"[1]
-      }
-      if(analysis == 2){
-        env.dist <- vegan::vegdist(envir, method = method.envir)
-        mod.obs <- vegan::mantel(p.dist, env.dist, permutations = runs)
-        statistic.obs <- mod.obs$statistic
-        p.site.shuffle <- mod.obs$signif
-      }
-    } else {
-      if(analysis == 1){
-        mod.obs <- vegan::adonis(p.dist~envir,permutations=runs, parallel = parallel)
-        statistic.obs <- mod.obs$aov.tab$F.Model[1]
-        p.site.shuffle <- mod.obs$aov.tab$"Pr(>F)"[1]
-      }
-      if(analysis == 2){
-        env.dist <- vegan::vegdist(envir, method = method.envir)
-        mod.obs <- vegan::mantel(p.dist, env.dist, permutations = runs, parallel = parallel)
-        statistic.obs <- mod.obs$statistic
-        p.site.shuffle <- mod.obs$signif
-      }
-    }
+  if(length(list.warning)>0){
+    res$list.warning <- list.warning
   }
-  RES$model <- mod.obs
-  RES$statistic.obs <- statistic.obs
-  RES$p.site.shuffle <- p.site.shuffle
-  seqpermutation <- SYNCSA::permut.vector(ncol(phylodist), nset = runs)
-  ptest <- function(samp, comm, phylodist, envir, method, squareroot, analysis){
-    MP.null <- SYNCSA::matrix.p(comm, phylodist[samp, samp], notification = FALSE)$matrix.P
-    dist.MP.null <- vegan::vegdist(MP.null, method = method)
-    if(squareroot){
-      dist.MP.null <- sqrt(dist.MP.null)
-    }
-    if(analysis == 1){
-      mod.null <- vegan::adonis(dist.MP.null~envir, permutations = 0)
-      res <- mod.null$aov.tab$F.Model[1]
-    } 
-    if(analysis == 2){
-      mod.null <- vegan::mantel(dist.MP.null, envir, permutations = 0)
-      res <- mod.null$statistic
-    }
-    return(res)
+  res.pcps.null <- matrix.p.null(comm, phylodist, runs = runs, calcpcps = FALSE)
+  res.pcps.null <- mutate.names.matrix.p.null(res.pcps.null, "pcps", newname)
+  res$P.obs <- res.pcps.null$P.obs
+  statistic.obs <- sapply(list(res.pcps.null$P.obs), FUN = FUN, simplify = FALSE, return.model = TRUE, envir = envir, ...)
+  res$model <- statistic.obs[[1]]$mod.obs
+  res$fun <- FUN
+  res$obs.statistic <- statistic.obs[[1]]$statistic.obs
+  newClusters <- FALSE
+  if (is.numeric(parallel)) {
+    parallel <- parallel::makeCluster(parallel, type = "PSOCK")
+    newClusters <- TRUE
   }
-  if(!is.null(parallel) & newClusters){
-    CL <- parallel::makeCluster(parallel, type = "PSOCK")
+  if (!inherits(parallel, "cluster")) {
+    statistic.null.taxa <- sapply(res.pcps.null$P.null.taxa, FUN = FUN, simplify = FALSE, envir = envir, ...)
+    statistic.null.site <- sapply(res.pcps.null$P.null.site, FUN = FUN, simplify = FALSE, envir = envir, ...)
   }
-  if(is.null(parallel)){
-    res.null <- matrix(NA,runs,1)
-    for (i in 1:runs) {
-      if(analysis == 1){
-        res.null[i,1] <- ptest(samp = seqpermutation[i,], comm = comm, phylodist = phylodist, envir = envir, method = method, squareroot = squareroot, analysis = analysis)   
-      }
-      if(analysis == 2){
-        res.null[i,1] <- ptest(samp = seqpermutation[i,], comm = comm, phylodist = phylodist, envir = env.dist, method = method, squareroot = squareroot, analysis = analysis)
-      }
-    }
-  } else {
-    if(analysis == 1){
-      res.null <- parallel::parRapply(CL, seqpermutation, ptest, comm = comm , phylodist = phylodist, envir = envir, method = method, squareroot = squareroot, analysis = analysis)
-    }
-    if(analysis == 2){
-      res.null <- parallel::parRapply(CL, seqpermutation, ptest, comm = comm , phylodist = phylodist, envir = env.dist, method = method, squareroot = squareroot, analysis = analysis)
-    }
+  else {
+    statistic.null.taxa <- parallel::parLapply(parallel, res.pcps.null$P.null.taxa, fun = FUN, envir = envir, ...)
+    statistic.null.site <- parallel::parLapply(parallel, res.pcps.null$P.null.site, fun = FUN, envir = envir, ...)
   }
-  if(!is.null(parallel) & newClusters){
-    parallel::stopCluster(CL)
+  if (newClusters) {
+    parallel::stopCluster(parallel)
   }
-  p.taxa.shuffle <- (sum(ifelse(res.null>=statistic.obs, 1, 0))+1)/(runs+1)
-  RES$p.taxa.shuffle <- p.taxa.shuffle
-  return(RES)
+  res$statistic.null.site <- do.call("rbind", statistic.null.site)
+  res$statistic.null.taxa <- do.call("rbind", statistic.null.taxa)
+  res$p.site.shuffle <- as.vector(rbind((apply(sweep(res$statistic.null.site, 2, res$obs.statistic, ">="), 2, sum)+1)/(runs + 1)))
+  res$p.taxa.shuffle <- as.vector(rbind((apply(sweep(res$statistic.null.taxa, 2, res$obs.statistic, ">="), 2, sum)+1)/(runs + 1)))
+  class(res) <- "pcpssig"
+  return(res)
 }
